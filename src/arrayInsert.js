b501f513310db2489d3f55522872a61bfe09b9ad
Ge Yang <yangge1987@gmail.com>
2016-07-30 11:01:27 -0700

    update README.md

---
+++ src/arrayInsert.js
@@ -0,0 +1,11 @@
+/** Created by ge on 7/30/16. */
+export default function arrayInsert (array, action) {
+  const {type, index, segment} = action;
+  if (action.type !== "i") return array;
+  if (!segment.length) return array;
+  return [
+    ...array.slice(0, index),
+    ...segment,
+    ...array.slice(index)
+  ]
+}
